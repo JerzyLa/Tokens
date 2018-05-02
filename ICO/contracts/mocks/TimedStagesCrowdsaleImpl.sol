0363b0d9416fa24810514c128f55e453ad5aaeb6
JerzyLa <jerzylasyk@gmail.com>
2018-05-02 19:33:34 +0200

    TimedStagesCrowdsale tests fixed after refactoring

---
+++ ICO/contracts/mocks/TimedStagesCrowdsaleImpl.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.21;
+
+import "../TimedStagesCrowdsale.sol";
+
+
+contract TimedStagesCrowdsaleImpl is TimedStagesCrowdsale {
+    function TimedStagesCrowdsaleImpl (
+        address _wallet,
+        ReleasableToken _token,
+        uint256 _rate1,
+        uint256 _minInvest1,
+        uint256 _openingTime1, 
+        uint256 _closingTime1,
+        uint256 _minInvest2,
+        uint256 _openingTime2, 
+        uint256 _closingTime2
+    )
+        public
+        TimedStagesCrowdsale(_wallet, _token)
+    {
+        stages.push(Stage(_rate1, _minInvest1, _openingTime1, _closingTime1, StageType.Standard));
+        stages.push(Stage(0, _minInvest2, _openingTime2, _closingTime2, StageType.PostDelivery));
+    }
+}
--- ICO/test/TimedStagesCrowdsale.test.js
+++ ICO/test/TimedStagesCrowdsale.test.js
@@ -11,19 +11,16 @@ require('chai')
   .use(require('chai-bignumber')(BigNumber))
   .should();
 
-const TimedStagesCrowdsale = artifacts.require('TimedStagesCrowdsale');
+const TimedStagesCrowdsaleImpl = artifacts.require('TimedStagesCrowdsaleImpl');
 const MediarToken = artifacts.require('MediarToken');
 
-contract('TimedStagesCrowdsale', function ([_, owner, investor, wallet]) {
-  const rate1 = new BigNumber(40000000);
-  const rate2 = new BigNumber(20000000);
-  const rate3 = new BigNumber(10000000);
-  const rate4 = new BigNumber(5000000);
+contract('TimedStagesCrowdsaleImpl', function ([_, owner, investor, wallet]) {
+  const rate1 = new BigNumber(100000000);
   const value = ether(1);
-  const tokenSupply = new BigNumber('2e26');
+  const minInvest1 = new BigNumber('5e17');
+  const minInvest2 = new BigNumber('2e17');
+  const tokenSupply = new BigNumber('4e26');
   const expectedTokenAmount1 = rate1.mul(value);
-  const expectedTokenAmount2 = rate2.mul(value);
-  const expectedTokenAmount3 = rate3.mul(value);
 
   before(async function () {
     // Advance to the next block to correctly read time in the solidity "now" function interpreted by ganache
@@ -33,23 +30,15 @@ contract('TimedStagesCrowdsale', function ([_, owner, investor, wallet]) {
   beforeEach(async function () {
     this.openingTime1 = latestTime() + duration.weeks(1);
     this.closingTime1 = this.openingTime1 + duration.weeks(1);
-    this.openingTime2 = this.closingTime1 + duration.weeks(1);
-    this.closingTime2 = this.openingTime2 + duration.weeks(1);
-    this.openingTime3 = this.closingTime2 + duration.weeks(1);
-    this.closingTime3 = this.openingTime3 + duration.weeks(1);
-    this.openingTime4 = this.closingTime3 + duration.weeks(1);
-    this.closingTime4 = this.openingTime4 + duration.weeks(1);
-    this.openingTimeLast = this.closingTime4 + duration.weeks(1);
+    this.openingTimeLast = this.closingTime1 + duration.weeks(1);
     this.closingTimeLast = this.openingTimeLast + duration.weeks(1);
     this.afterClosingTime = this.closingTimeLast + duration.seconds(1);
     
     this.token = await MediarToken.new({ from: owner });
-    this.crowdsale = await TimedStagesCrowdsale.new(wallet, this.token.address,
-      rate1, this.openingTime1, this.closingTime1,
-      rate2, this.openingTime2, this.closingTime2,
-      rate3, this.openingTime3, this.closingTime3,
-      rate4, this.openingTime4, this.closingTime4,
-      this.openingTimeLast, this.closingTimeLast);
+    this.crowdsale = await TimedStagesCrowdsaleImpl.new(wallet, this.token.address, 
+      rate1, minInvest1, this.openingTime1, this.closingTime1,
+      minInvest2, this.openingTimeLast, this.closingTimeLast
+    );
 
     await this.token.setTransferAgent(owner, true, { from: owner });
     await this.token.setTransferAgent(this.crowdsale.address, true, { from: owner });
@@ -74,7 +63,7 @@ contract('TimedStagesCrowdsale', function ([_, owner, investor, wallet]) {
       await increaseTimeTo(this.openingTime1);
       let ended = await this.crowdsale.hasClosed();
       ended.should.equal(false);
-      await this.crowdsale.buyTokens({ value: value*5, from: investor });
+      await this.crowdsale.buyTokens({ value: value*4, from: investor });
       const balance = await this.token.balanceOf(this.crowdsale.address);
       balance.should.be.bignumber.equal(0);
       ended = await this.crowdsale.hasClosed();
@@ -93,64 +82,42 @@ contract('TimedStagesCrowdsale', function ([_, owner, investor, wallet]) {
       await this.crowdsale.buyTokens({ from: investor, value: value }).should.be.rejectedWith(EVMRevert);
     });
 
-    it('should accept payments after start first phase', async function () {
+    it('should accept payments after start first stage', async function () {
       await increaseTimeTo(this.openingTime1);
       await this.crowdsale.send(value).should.be.fulfilled;
       await this.crowdsale.buyTokens({ value: value, from: investor }).should.be.fulfilled;
     });
 
-    it('should accept payments after start second phase', async function () {
-      await increaseTimeTo(this.openingTime2);
-      await this.crowdsale.send(value).should.be.fulfilled;
-      await this.crowdsale.buyTokens({ value: value, from: investor }).should.be.fulfilled;
-    });
-
-    it('should accept payments after start third phase', async function () {
-      await increaseTimeTo(this.openingTime3);
-      await this.crowdsale.send(value).should.be.fulfilled;
-      await this.crowdsale.buyTokens({ value: value, from: investor }).should.be.fulfilled;
-    });
-
-    it('should accept payments after start fourth phase', async function () {
-      await increaseTimeTo(this.openingTime4);
-      await this.crowdsale.send(value).should.be.fulfilled;
-      await this.crowdsale.buyTokens({ value: value, from: investor }).should.be.fulfilled;
-    });
-
-    it('should accept payments after start last phase', async function () {
+    it('should accept payments after start last stage', async function () {
       await increaseTimeTo(this.openingTimeLast);
       await this.crowdsale.send(value).should.be.fulfilled;
       await this.crowdsale.buyTokens({ value: value, from: investor }).should.be.fulfilled;
     });
 
-    it('should reject payments after end of first phase', async function () {
+    it('should reject payments after end of first stage', async function () {
       await increaseTimeTo(this.afterClosingTime1);
       await this.crowdsale.send(value).should.be.rejectedWith(EVMRevert);
       await this.crowdsale.buyTokens({ value: value, from: investor }).should.be.rejectedWith(EVMRevert);
     });
 
-    it('should reject payments after end of second phase', async function () {
-      await increaseTimeTo(this.afterClosingTime2);
-      await this.crowdsale.send(value).should.be.rejectedWith(EVMRevert);
-      await this.crowdsale.buyTokens({ value: value, from: investor }).should.be.rejectedWith(EVMRevert);
-    });
-
-    it('should reject payments after end of third phase', async function () {
-      await increaseTimeTo(this.afterClosingTime3);
-      await this.crowdsale.send(value).should.be.rejectedWith(EVMRevert);
-      await this.crowdsale.buyTokens({ value: value, from: investor }).should.be.rejectedWith(EVMRevert);
-    });
-
-    it('should reject payments after end of fourth phase', async function () {
-      await increaseTimeTo(this.afterClosingTime4);
-      await this.crowdsale.send(value).should.be.rejectedWith(EVMRevert);
-      await this.crowdsale.buyTokens({ value: value, from: investor }).should.be.rejectedWith(EVMRevert);
-    });
-
-    it('should reject payments after end of last phase', async function () {
+    it('should reject payments after end of last stage', async function () {
       await increaseTimeTo(this.afterClosingTimeLast);
       await this.crowdsale.send(value).should.be.rejectedWith(EVMRevert);
       await this.crowdsale.buyTokens({ value: value, from: investor }).should.be.rejectedWith(EVMRevert);
+    });
+
+    it('should reject payments when invest less than minimum after start first stage', async function () {
+      await increaseTimeTo(this.openingTime1);
+      const tooLowInvest = minInvest1 - 100;
+      await this.crowdsale.send(tooLowInvest).should.be.rejectedWith(EVMRevert);
+      await this.crowdsale.buyTokens({ value: tooLowInvest, from: investor }).should.be.rejectedWith(EVMRevert);
+    });
+
+    it('should reject payments when invest less than minimum after start last stage', async function () {
+      await increaseTimeTo(this.openingTimeLast);
+      const tooLowInvest = minInvest2 - 100;
+      await this.crowdsale.send(tooLowInvest).should.be.rejectedWith(EVMRevert);
+      await this.crowdsale.buyTokens({ value: tooLowInvest, from: investor }).should.be.rejectedWith(EVMRevert);
     });
   });
 
@@ -172,35 +139,21 @@ contract('TimedStagesCrowdsale', function ([_, owner, investor, wallet]) {
       event.args.value.should.be.bignumber.equal(value);
     });
 
-    it('should assign tokens to beneficiary in phase1', async function () {
+    it('should assign tokens to beneficiary in stage1', async function () {
       await increaseTimeTo(this.openingTime1);
       await this.crowdsale.buyTokens({ value, from: investor });
       const balance = await this.token.balanceOf(investor);
       balance.should.be.bignumber.equal(expectedTokenAmount1);
     });
 
-    it('should assign tokens to beneficiary in phase2', async function () {
-      await increaseTimeTo(this.openingTime2);
-      await this.crowdsale.buyTokens({ value, from: investor });
-      const balance = await this.token.balanceOf(investor);
-      balance.should.be.bignumber.equal(expectedTokenAmount2);
-    });
-
-    it('should assign tokens to beneficiary in phase3 ', async function () {
-      await increaseTimeTo(this.openingTime3);
-      await this.crowdsale.buyTokens({ value, from: investor });
-      const balance = await this.token.balanceOf(investor);
-      balance.should.be.bignumber.equal(expectedTokenAmount3);
-    });
-
-    it('should not assign tokens to beneficiary in last phase (token delivery postponed)', async function () {
+    it('should not assign tokens to beneficiary in last stage (token delivery postponed)', async function () {
       await increaseTimeTo(this.openingTimeLast);
       await this.crowdsale.buyTokens({ value, from: investor });
       const balance = await this.token.balanceOf(investor);
       balance.should.be.bignumber.equal(0);
     });
 
-    it('should forward funds to wallet in phase 1 after purchase', async function () {
+    it('should forward funds to wallet in stage 1 after purchase', async function () {
       await increaseTimeTo(this.openingTime1);
       const pre = web3.eth.getBalance(wallet);
       await this.crowdsale.buyTokens({ value, from: investor });
@@ -208,23 +161,7 @@ contract('TimedStagesCrowdsale', function ([_, owner, investor, wallet]) {
       post.minus(pre).should.be.bignumber.equal(value);
     });
 
-    it('should forward funds to wallet in phase 2 after purchase', async function () {
-      await increaseTimeTo(this.openingTime2);
-      const pre = web3.eth.getBalance(wallet);
-      await this.crowdsale.buyTokens({ value, from: investor });
-      const post = web3.eth.getBalance(wallet);
-      post.minus(pre).should.be.bignumber.equal(value);
-    });
-
-    it('should forward funds to wallet in phase 3 after purchase', async function () {
-      await increaseTimeTo(this.openingTime3);
-      const pre = web3.eth.getBalance(wallet);
-      await this.crowdsale.buyTokens({ value, from: investor });
-      const post = web3.eth.getBalance(wallet);
-      post.minus(pre).should.be.bignumber.equal(value);
-    });
-
-    it('should forward funds to wallet in Last phase (token delivery postponed)', async function () {
+    it('should forward funds to wallet in Last stage (token delivery postponed)', async function () {
       await increaseTimeTo(this.openingTimeLast);
       const pre = web3.eth.getBalance(wallet);
       await this.crowdsale.buyTokens({ value, from: investor });
