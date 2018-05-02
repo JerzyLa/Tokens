93086b522e7633a2d1bff509f79ffad9d6fea7d1
JerzyLa <jerzylasyk@gmail.com>
2018-05-02 14:06:53 +0200

    fix not working tests

--- ICO/migrations/2_deploy_contracts.js
+++ ICO/migrations/2_deploy_contracts.js
@@ -2,13 +2,13 @@ var MediarToken = artifacts.require("./MediarToken.sol");
 var MediarCrowdsale = artifacts.require("./MediarCrowdsale.sol");
 
 module.exports = function(deployer, network, accounts) {
-   deployer.deploy(MediarToken).then(() => {
-       return deployer.deploy(MediarCrowdsale, accounts[0], MediarToken.address, 100000000000000000, 
-        4000, 1524560400, 1524571200, 
-        3000, 1524574800, 1524585600, 
-        2000, 1524589200, 1524600000,
-        1524603600, 1524686400);
-   });
+//    deployer.deploy(MediarToken).then(() => {
+//        return deployer.deploy(MediarCrowdsale, accounts[0], MediarToken.address, 100000000000000000, 
+//         4000, 1524560400, 1524571200, 
+//         3000, 1524574800, 1524585600, 
+//         2000, 1524589200, 1524600000,
+//         1524603600, 1524686400);
+//    });
 
    // TODO: dodać przsył tokenow
 };
