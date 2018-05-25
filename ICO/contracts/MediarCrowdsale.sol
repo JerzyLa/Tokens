15bfd73071e7d9d2124c8e20288c15e5f7739e0f
Jerzy <jerzylasyk@gmail.com>
2018-05-25 23:40:43 +0200

    update dates in sm

--- ICO/contracts/MediarCrowdsale.sol
+++ ICO/contracts/MediarCrowdsale.sol
@@ -29,7 +29,7 @@ contract MediarCrowdsale is PostDeliveryCrowdsale, RefundableCrowdsale {
         public
         TimedStagesCrowdsale(_wallet, _token)
     {
-        stages.push(Stage(4000, 500 finney, 1526342400, 1529020800));
+        stages.push(Stage(4000, 500 finney, 1527811200, 1529020800));
         stages.push(Stage(3000, 200 finney, 1529107200, 1531699200));
         stages.push(Stage(2500, 200 finney, 1533254400, 1534723200));
         stages.push(Stage(2000, 200 finney, 1535673600, 1537142400));
